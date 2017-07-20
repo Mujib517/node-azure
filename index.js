8b40d5f7b3235eaa948ebc8bc06c96dfd58ca89b
rahmam <mohammed.rahman@cdk.com>
2017-07-20 17:39:12 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+
+let app = express();
+
+let port = process.env.port | 3000;
+
+app.listen(3000, function () {
+    console.log("server running on port 3000");
+});
+
+app.get('/', function (req, res) {
+    res.status(200);
+    res.send("API Up and Running");
+});
+
+
